91ccdf3073410ae12016f742851eb7bc34555341
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-05 17:26:23 -0600

    Agregar archivos de proyecto.

---
+++ EjecicioClase/Program.cs
@@ -0,0 +1,16 @@
+﻿using EjecicioClase;
+
+SuperHeroe super = new SuperHeroe();
+SuperHeroe superDno = new SuperHeroe();
+SuperHeroe supeTres = new SuperHeroe();
+
+
+super.nombre="superman";
+super.IdentidadSecreta = " Clrk Kent";
+super.ciudad = "Metropolis";
+super.puedeVolar = true;
+
+Console.WriteLine($"Nombre: {super.nombre}");
+Console.WriteLine($"Identidad Secreta: {super.IdentidadSecreta}");
+Console.WriteLine($"Ciudad: {super.ciudad}");
+Console.WriteLine($"Puede Volar: {super.puedeVolar}");
---
+++ EjecicioClase/SuperHeroe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjecicioClase
+{
+    internal class SuperHeroe
+    {
+        public string? nombre;
+        public string? IdentidadSecreta;
+        public string? ciudad;
+        public bool? puedeVolar;
+
+    }
+}
